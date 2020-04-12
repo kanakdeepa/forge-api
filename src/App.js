fe0aeff86ad30a143543fd34509b2ff2183b5014
kanakdeepa <kanakdeepa.pradhan91@gmail.com>
2020-04-12 14:46:09 +0800

    Fixed issue with header CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { navItems } from './config/nav';
 import './scss/layout.scss';
 
@@ -9,13 +9,16 @@ const App = () => {
       <Router>
         <Switch>
           {navItems.map((item) => (
-            <Route path={item.url} key={item.url}>
+            <Route exact path={item.url} key={item.url}>
               <header>
                 <div className="header-content">{item.label}</div>
               </header>
               {item.component}
             </Route>
           ))}
+          <Route exact path="/">
+            <Redirect to="/developer/documentation" />
+          </Route>
         </Switch>
       </Router>
     </div>
