78798c13392d2399ff62811e1b06a0f48c6559ba
kanakdeepa <kanakdeepa.pradhan91@gmail.com>
2020-04-12 15:43:52 +0800

    Added loader

--- src/components/Documentation/Documentation.js
+++ src/components/Documentation/Documentation.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './Documentation.scss';
 import { getApis } from '../../services/apiService';
 import Card from '../Card/Card';
+import Loader from '../../images/loader.gif';
 
 const Documentation = () => {
   const [apis, setApis] = useState(null);
@@ -16,18 +17,23 @@ const Documentation = () => {
   }, []);
 
   return (
-    <div className="documentation-container">
+    <div className={`documentation-container ${apis ? 'documentation-bg' : ''}`}>
       <div className="page-description">
         <h2>APIs &amp; Services</h2>
         <p>Access information on how to use Forge APIs and Services</p>
       </div>
       <div className="cards-section">
-        {apis &&
+        {apis ? (
           apis.map((api) => (
             <div className="card-wrapper">
               <Card content={api} />
             </div>
-          ))}
+          ))
+        ) : (
+          <div className="loader-wrapper">
+            <img src={Loader} alt="loader" />
+          </div>
+        )}
       </div>
     </div>
   );
